01dd53a41f8d9a620e15d3d04f58b55ce9de15de
Alan Sato <Alan.Sato@toyota.com>
2018-11-02 16:34:44 -0500

    remove 2 annotations

--- src/main/java/com/sam/rest/myrestapi3/Myrestapi3Application.java
+++ src/main/java/com/sam/rest/myrestapi3/Myrestapi3Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.sam.rest.myrestapi3.daos")
-@EntityScan("com.sam.rest.myrestapi3.beans")
 @SpringBootApplication
 public class Myrestapi3Application {
 
